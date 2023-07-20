368b453d1d345a223bdddb91b2851ddee55cebc7
chandn33 <misschandni_patel003@hotmail.co.uk>
2023-07-20 23:10:00 +0100

    removed irrelevant files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+import React from "react";
+import Weather from "./Weather";
 import './App.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="container">
+      
+      <Weather defaultCity="Leicester" />
+    
+    <footer>
+    Open-source on {""}
+      <a href="https://github.com/chandn33/react-weather-app" target="_blank" rel="noopener noreferrer">
+        GitHub</a> | Project designed by Chandni Patel🌻
+    </footer>
+    </div>
     </div>
   );
 }
-
-export default App;
---
+++ src/Weather.js
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+import WeatherInfo from "./WeatherInfo";
+import WeatherForecast from "./WeatherForecast";
+import axios from "axios";
+import "./Weather.css";
+
+export default function Weather(props) {
+    const [weatherData, setWeatherData] = useState({ ready: false });
+    const [city, setCity] = useState(props.defaultCity);
+    const weatherIconCode = "10d";
+
+    function handleResponse(response) {
+    setWeatherData({
+        ready: true,
+        coordinates: response.data.coord,
+        temperature: response.data.main.temp,
+        humidity: response.data.main.humidity,
+        wind: response.data.wind.speed,
+        description: response.data.weather[0].description,
+        city: response.data.name,
+        date: new Date(response.data.dt * 1000),
+        icon: `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    });
+    
+}
+
+function search() {
+    const apiKey = "13c250c9139660bdb6fb8b5c81e33bd0";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+}
+
+function handleSubmit(event) {
+    event.preventDefault();
+    search();
+}
+
+function handleCityChange(event) {
+setCity(event.target.value);
+}
+
+if (weatherData.ready) {
+    return (
+    <div className="Weather">
+    <form onSubmit={handleSubmit}>
+        <div className="row">
+            <div className="col-9">
+        <input type="search" placeholder="Enter a city..."
+        className="form-control" autoFocus="on"
+        onChange={handleCityChange}
+        />
+        </div>
+        <div className="col-3">
+        <input type="submit" value="Search" className="btn btn-primary w-100" />
+        
+    
+        </div>
+        </div>
+    </form>
+    <WeatherInfo data={weatherData} />
+    <WeatherForecast coordinates={weatherData.coordinates} />
+    </div>
+); 
+} else {
+    search();    
+    return "Loading...";
+}
+}
